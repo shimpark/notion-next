54fc4d6254fabf250e5cb515ff1b9de72cca6839
luckshim <shimpark@gmail.com>
2024-06-13 02:23:19 +0900

    site.config changed

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config';
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'd1e89e9e42eb4ebf9486ae0374039efc',
+  rootNotionPageId: '2a51573987054c8cb016242b4b9c3952',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -54,11 +54,11 @@ export default siteConfig({
   navigationLinks: [
     {
       title: '카테고리',
-      pageId: '36400db511474331b5c1de6918212469',
+      pageId: '2a51573987054c8cb016242b4b9c3952',
     },
     {
       title: '카테고리',
-      pageId: '36400db511474331b5c1de6918212469',
+      pageId: '2a51573987054c8cb016242b4b9c3952',
       menuPage: true,
     },
   ],
